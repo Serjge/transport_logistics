5944a3a1e09bf5efb203ba402d46837ff908102f
Serjge <serjge@yandex.ru>
2022-05-01 21:35:35 +0300

    feat:add test actions setNewWarehouses, removeMapMark, setMapMark, isResizeMap

--- src/store/reducers/__Test__/appReducer.test.ts
+++ src/store/reducers/__Test__/appReducer.test.ts
@@ -1,14 +1,64 @@
+import { LocationAddressType } from 'api/type';
 import { Element } from 'enum';
-import { changeWarehouse, isActiveRout, setError, setRoutes } from 'store/actions';
+import {
+  changeWarehouse,
+  isActiveRout,
+  isResizeMap,
+  removeMapMark,
+  setError,
+  setMapMark,
+  setNewWarehouses,
+  setRoutes,
+} from 'store/actions';
 import { appReducer, InitialAppStateType } from 'store/reducers/appReducer';
 
 let initialState: InitialAppStateType;
 
 const LENGTH_ARRAY_SIX = 6;
+const LAST_ELEMENT = 1;
 const secondOrder = '2';
 const thirdOrder = '3';
 const warehouseId = '3';
 const error = 'ERROR';
+const city = 'Pskov';
+const state = 'RU';
+const street = 'Lenina';
+const point = {
+  lat: 57.83342,
+  lng: 28.319382,
+};
+
+const location: LocationAddressType = {
+  street: 'Текстильный переулок',
+  adminArea6: '',
+  adminArea6Type: 'Neighborhood',
+  adminArea5: 'Псков',
+  adminArea5Type: 'City',
+  adminArea4: '',
+  adminArea4Type: 'County',
+  adminArea3: 'Псковская область',
+  adminArea3Type: 'State',
+  adminArea1: 'RU',
+  adminArea1Type: 'Country',
+  postalCode: '180000',
+  geocodeQualityCode: 'B1AAA',
+  geocodeQuality: 'STREET',
+  dragPoint: false,
+  sideOfStreet: 'N',
+  linkId: 0,
+  unknownInput: '',
+  type: 's',
+  latLng: {
+    lat: 57.83342,
+    lng: 28.319382,
+  },
+  displayLatLng: {
+    lat: 57.83342,
+    lng: 28.319382,
+  },
+  mapUrl:
+    'http://www.mapquestapi.com/staticmap/v5/map?key=FVfSyrceb9BJLn1vHom7SMKhdJnFGpCL&type=map&size=225,160&locations=57.833420376958344,28.31938164506402|marker-sm-50318A-1&scalebar=true&zoom=15&rand=-421367275',
+};
 
 const routes = [
   { lng: 28.368454, lat: 57.808624 },
@@ -108,7 +158,6 @@ beforeEach(() => {
 describe('app reducer action changeWarehouse', () => {
   test('change loading warehouse', () => {
     const action = changeWarehouse(secondOrder, warehouseId, 'loading');
-
     const endState = appReducer(initialState, action);
 
     expect(endState.orders[Element.First].loadingWarehouseId).toBe(warehouseId);
@@ -116,7 +165,6 @@ describe('app reducer action changeWarehouse', () => {
   });
   test('change unloading warehouse', () => {
     const action = changeWarehouse(secondOrder, warehouseId, 'unloading');
-
     const endState = appReducer(initialState, action);
 
     expect(endState.orders[Element.First].loadingWarehouseId).not.toBe(warehouseId);
@@ -127,7 +175,6 @@ describe('app reducer action changeWarehouse', () => {
 describe('app reducer action setRouts', () => {
   test('set routes ', () => {
     const action = setRoutes(secondOrder, routes);
-
     const endState = appReducer(initialState, action);
 
     expect(endState.orders[Element.First].routes.length).toBe(LENGTH_ARRAY_SIX);
@@ -140,7 +187,6 @@ describe('app reducer action setRouts', () => {
 describe('app reducer action isActiveRout', () => {
   test('change route active true', () => {
     const action = isActiveRout(secondOrder, true);
-
     const endState = appReducer(initialState, action);
 
     expect(endState.orders[Element.First].isActive).toBe(true);
@@ -149,7 +195,6 @@ describe('app reducer action isActiveRout', () => {
 
   test('change route active false', () => {
     const action = isActiveRout(thirdOrder, false);
-
     const endState = appReducer(initialState, action);
 
     expect(initialState.orders[Element.Second].isActive).toBe(true);
@@ -161,7 +206,6 @@ describe('app reducer action isActiveRout', () => {
 describe('app reducer action setError', () => {
   test('set error message', () => {
     const action = setError(error);
-
     const endState = appReducer(initialState, action);
 
     expect(endState.error).toBe(error);
@@ -171,10 +215,53 @@ describe('app reducer action setError', () => {
   test('remove error message', () => {
     initialState.error = error;
     const action = setError(null);
-
     const endState = appReducer(initialState, action);
 
     expect(endState.error).toBe(null);
     expect(initialState.error).toBe(error);
+  });
+});
+
+describe('app reducer action isResizeMap', () => {
+  test('change isResize true', () => {
+    const action = isResizeMap(true);
+    const endState = appReducer(initialState, action);
+
+    expect(endState.isResize).toBe(true);
+    expect(initialState.isResize).toBe(false);
+  });
+});
+
+describe('app reducer action setMapMark', () => {
+  test('set map mark', () => {
+    const action = setMapMark(location);
+    const endState = appReducer(initialState, action);
+
+    expect(endState.mapMark?.street).toBe(location.street);
+    expect(initialState.mapMark).toBe(null);
+  });
+});
+
+describe('app reducer action removeMapMark', () => {
+  test('remove map mark', () => {
+    initialState.mapMark = location;
+
+    const action = removeMapMark();
+    const endState = appReducer(initialState, action);
+
+    expect(endState.mapMark).toBe(null);
+    expect(initialState.mapMark).not.toBe(null);
+    expect(initialState.mapMark).toBe(location);
+  });
+});
+
+describe('app reducer action setNewWarehouses', () => {
+  test('set new warehouses ', () => {
+    const action = setNewWarehouses(city, street, state, point);
+    const endState = appReducer(initialState, action);
+    const lastWarehouses = endState.warehouses.length - LAST_ELEMENT;
+
+    expect(endState.warehouses[lastWarehouses].city).toBe(city);
+    expect(initialState.warehouses.length).not.toBe(endState.warehouses.length);
   });
 });
